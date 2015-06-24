dd612ee2563413f259efdc365e97bc3b9912b748
Jonathan Smith <jonathan@peoplesized.com>
2015-06-24 14:10:36 +0100

    basic login

--- routes.py
+++ routes.py
@@ -1,16 +1,5 @@
 
 
-
-# Create anti-forgery state token
-# @app.route('/login')
-# def showLogin():
-#     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
-#                     for x in xrange(32))
-#     login_session['state'] = state
-#     # return "The current session state is %s" % login_session['state']
-#     return render_template('login.html', STATE=state)
-
-
 # # JSON APIs to view Restaurant Information
 # @app.route('/restaurant/<int:restaurant_id>/menu/JSON')
 # def restaurantMenuJSON(restaurant_id):
