c347943f4c60e1359fc80f9ce80cdff3d27e3758
Jonathan Smith <jonathan@peoplesized.com>
2015-06-24 01:22:14 +0100

    database setup and seeding

---
+++ database_setup.py
@@ -0,0 +1,60 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ Simple User class, created by third-party authentication"""
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+
+class Category(Base):
+    """
+    Categories are owned by one User, and only the owner can edit/delete a
+    Category.
+    Categories have many Items.
+    """
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+class Item(Base):
+    """
+    Items are owned by one User, and only the owner can edit/delete an Item.
+    Items have names, and belong to a Category.
+    """
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+engine = create_engine('sqlite:///category.db')
+
+
+Base.metadata.create_all(engine)
---
+++ seed_database.py
@@ -0,0 +1,47 @@
+import json
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Base, User, Category, Item
+
+engine = create_engine('sqlite:///category.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+
+def create_record(seed, seed_type):
+    """
+    Abstract creation of seed record from the given `dictionary`
+    and `seed_type`
+    """
+    # Create a constructor from the imported User, Category or Item classes
+    constructor = globals()[seed_type]
+    record = constructor(**seed)
+    session.add(record)
+    session.commit()
+
+if __name__ == '__main__':
+    # Load the `seeds.json` file, and iterate, creating seed records for each
+    # type of record.
+    with open('seeds.json') as seeds_json:
+        seeds = json.load(seeds_json)
+
+        # Use this Array to maintain order of creation
+        seed_types = ['User', 'Category', 'Item']
+
+        for seed_type in seed_types:
+            for seed in seeds[seed_type]:
+                create_record(seed, seed_type)
+
+    print "Seeded User, Category and Item records"
